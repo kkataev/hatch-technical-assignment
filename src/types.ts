b8af094637e0c05781010e534690fa345a774c52
Neo <alghoochi@gmail.com>
2021-08-22 19:25:28 +0400

    ViewMode, City, Size types

---
+++ src/types.ts
@@ -0,0 +1,15 @@
+export enum ViewMode {
+  Grid = 'grid',
+  List = 'list',
+}
+
+export type City = {
+  city: string,
+  adminName: string,
+  population: number,
+};
+
+export type Size = {
+  height: number,
+  width: number,
+};
